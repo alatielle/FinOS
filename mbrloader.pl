eca104c5f7fbd43c82ce94557c8fc7bc597e729d
Elena Rashkovan <mi.usu@ya.ru>
2011-10-12 22:12:27 +0600

    version1

---
+++ mbrloader.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+my $file = $ARGV[0];
+my $output = "output";
+my $loader;
+my $stream;
+open my $F, '<', $file or die "Can't open the file $file: $!";
+open my $O, '>', $output;
+binmode $F;
+binmode $O;
+die("Failed to read from $file") if (read( $F, $stream, 512 ) != 512);
+$_=unpack "H4",substr($stream,510,2);
+if ($_ =~ /55aa/)
+{
+    print $O $stream;
+    exit 0;
+}
+close $F;
+close $O;
