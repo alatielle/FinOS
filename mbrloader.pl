2d83ccadb1e759d714def9d74d8d9e46efc2a49a
U-Александр-ПК\Александр <@-.(none)>
2011-10-12 22:45:34 +0600

    disassemled BR added

--- mbrloader.pl
+++ mbrloader.pl
@@ -1,4 +1,6 @@
 #!/usr/bin/perl
+#use strict;
+#use warnings;
 my $file = $ARGV[0];
 my $output = "output";
 my $loader;
@@ -11,8 +13,22 @@ die("Failed to read from $file") if (read( $F, $stream, 512 ) != 512);
 $_=unpack "H4",substr($stream,510,2);
 if ($_ =~ /55aa/)
 {
-    print $O $stream;
-    exit 0;
+	print $O $stream;
+	exit 0;
+}
+my $flag=0;
+while(<$F>){
+$_=unpack "H2",$_;
+if ($flag) {
+    $flag=0;
+    if ($_ =~ /aa/) {
+        seek($F, -512, 1);
+        read($F, $stream, 512);
+        print $O $stream;
+        exit 0;
+    }
+}
+$flag=1 if ($_ =~ /55/);
 }
 close $F;
 close $O;
