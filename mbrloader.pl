bde62a625995839c03ff56d9472d54d50da0f8df
Elena Rashkovan <mi.usu@ya.ru>
2011-10-13 16:26:00 +0600

    version3

--- mbrloader.pl
+++ mbrloader.pl
@@ -11,8 +11,28 @@ die("Failed to read from $file") if (read( $F, $stream, 512 ) != 512);
 $_=unpack "H4",substr($stream,510,2);
 if ($_ =~ /55aa/)
 {
-    print $O $stream;
-    exit 0;
+	print $O $stream;
+	close $F;
+	close $O;
+	system("ndisasm $output > dis.txt") == 0
+		or die("ndisasm failure: $?");
+	exit 0;
 }
-close $F;
-close $O;
+my $flag=0;
+while(<$F>){
+$_=unpack "H2",$_;
+if ($flag) {
+    $flag=0;
+    if ($_ =~ /aa/) {
+        seek($F, -512, 1);
+        read($F, $stream, 512);
+        print $O $stream;
+	close $F;
+	close $O;
+	system("ndisasm $output > dis.txt") == 0
+		or die("ndisasm failure: $?");
+	exit 0;
+    }
+}
+$flag=1 if ($_ =~ /55/);
+}
