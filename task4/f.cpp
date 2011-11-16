d8c21c63a43ff69800bac56047e81494a87d3cee
U-Александр-ПК\Александр <@-.(none)>
2011-11-17 03:02:02 +0600

    Skeleton

--- task4/f.cpp
+++ task4/f.cpp
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+voif f(double arg)
+{
+	printf("%f",arg);
+	return;
+}
+
+extern __inline__ double g(int arg0, double arg1);
+
 int main()
 {
+	f(g(10,0.5));
 	return 0;
 }
