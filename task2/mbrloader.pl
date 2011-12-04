44557e79382797930ef17aea9d144571134bc89a
Elena Rashkovan <mi.usu@ya.ru>
2011-12-04 23:00:04 +0600

    fixed v1

--- task2/mbrloader.pl
+++ task2/mbrloader.pl
@@ -3,36 +3,26 @@ my $file = $ARGV[0];
 my $output = "output";
 my $loader;
 my $stream;
+die "Argument needed!" if ( scalar(@ARGV) == 0 );
 open my $F, '<', $file or die "Can't open the file $file: $!";
 open my $O, '>', $output;
 binmode $F;
 binmode $O;
-die("Failed to read from $file") if (read( $F, $stream, 512 ) != 512);
-$_=unpack "H4",substr($stream,510,2);
-if ($_ =~ /55aa/)
-{
-print $O $stream;
-close $F;
-close $O;
-system("ndisasm $output > dis.txt") == 0
-or die("ndisasm failure: $?");
-exit 0;
+die "Failed to read from $file" if ( (read $F, $stream, 510) != 510 );
+while( read $F, $_, 1 ) {
+	$_ = unpack "H2", $_;
+	if ( $_ =~ /55/ ) {
+		die "Failed to read from $file" if ( (read $F, $_, 1) != 1 );
+		$_ = unpack "H2", $_;
+		if ( $_ =~ /aa/ ) {
+			die "Failed to change position in $file" if ( (seek $F, -512, 1) != 1 );
+			die "Failed to read from $file" if ( (read $F, $stream, 512) != 512 );
+			print $O $stream;
+			close $F;
+			close $O;
+			system ( "ndisasm $output > dis.txt" ) == 0	or die("ndisasm failure: $?");
+			exit 0;
+		}
+	}
 }
-my $flag=0;
-while(<$F>){
-$_=unpack "H2",$_;
-if ($flag) {
-    $flag=0;
-    if ($_ =~ /aa/) {
-        seek($F, -512, 1);
-        read($F, $stream, 512);
-        print $O $stream;
-close $F;
-close $O;
-system("ndisasm $output > dis.txt") == 0
-or die("ndisasm failure: $?");
-exit 0;
-    }
-}
-$flag=1 if ($_ =~ /55/);
-}
+die "Failed to find master boot record in $file: $!";
