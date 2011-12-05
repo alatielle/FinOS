ecc66e25834079fd1aa9fff9b4cef24181dcddea
Elena Rashkovan <mi.usu@ya.ru>
2011-12-05 15:24:45 +0600

    fixed v2

--- task2/mbrloader.pl
+++ task2/mbrloader.pl
@@ -3,26 +3,27 @@ my $file = $ARGV[0];
 my $output = "output";
 my $loader;
 my $stream;
+my $found = 0;
 die "Argument needed!" if ( scalar(@ARGV) == 0 );
 open my $F, '<', $file or die "Can't open the file $file: $!";
-open my $O, '>', $output;
 binmode $F;
-binmode $O;
 die "Failed to read from $file" if ( (read $F, $stream, 510) != 510 );
-while( read $F, $_, 1 ) {
+while( (read $F, $_, 1)==1 ) {
 	$_ = unpack "H2", $_;
-	if ( $_ =~ /55/ ) {
+	if ( $_ eq "55" ) {
 		die "Failed to read from $file" if ( (read $F, $_, 1) != 1 );
 		$_ = unpack "H2", $_;
-		if ( $_ =~ /aa/ ) {
+		if ( $_ eq "aa" ) {
+			$found++;
+			open my $O, '>', $output;
+			binmode $O;
 			die "Failed to change position in $file" if ( (seek $F, -512, 1) != 1 );
 			die "Failed to read from $file" if ( (read $F, $stream, 512) != 512 );
 			print $O $stream;
-			close $F;
 			close $O;
-			system ( "ndisasm $output > dis.txt" ) == 0	or die("ndisasm failure: $?");
-			exit 0;
+			system ( 'ndisasm '.$output.' > dis'.$found.'.txt' ) == 0 or die("ndisasm failure: $?");
 		}
 	}
 }
-die "Failed to find master boot record in $file: $!";
+close $F;
+die "Failed to find master boot record in $file: $!" if !$found;
