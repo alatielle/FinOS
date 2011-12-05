2f1264f40215856c0f9b4956941ec3d7912ecb3c
Elena Rashkovan <mi.usu@ya.ru>
2011-12-05 16:45:17 +0600

    fixed v1

--- task4/4.2/g.asm
+++ task4/4.2/g.asm
@@ -1,7 +1,16 @@
 global  _g
+extern	_f
+	section .data
+dbl dq 0
         section .text
 _g:
 finit
 fld qword [esp+4]
 fimul dword [esp+12]
+fstp qword [dbl]
+
+push	dword [dbl+4]
+push	dword [dbl]
+call	_f
+add	sp,8
 ret
